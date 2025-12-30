c3ec33ee5b7a5f3bf76948591011c8829858703b
Abhay Madhuwan <abhii24502@gmail.com>
2025-12-30 16:35:14 +0530

    on behalf of new updates

--- screens/NinjaEatsProfileScreen.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-export default function NinjaEatsProfileScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Ninja Eats Profile</Text>
-    </View>
-  );
-}
